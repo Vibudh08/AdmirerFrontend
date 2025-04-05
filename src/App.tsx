118da0cb31c9c7fd56add0aada71601474786575
yash raj mishra <yashraj06112001@gmail.com>
2025-04-05 10:42:16 +0530

    removed header and Footer for login and SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SignUp from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 
 function App() {
+  const [currentRoute, setCurrentRoute] = useState("/");
+  function RouteTracker() {
+    const location = useLocation();
+
+    useEffect(() => {
+      setCurrentRoute(location.pathname);
+    }, [location]);
+
+    return null;
+  }
+
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <RouteTracker />
+        {currentRoute !== "/SignUp" && currentRoute !== "/LogIn" && <Header />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/LogIn" element={<Login/>}/>
+          <Route path="/LogIn" element={<Login />} />
         </Routes>
-        <Footer />
+        {currentRoute !== "/SignUp" && currentRoute !== "/LogIn" && <Footer />}
       </BrowserRouter>
     </>
   );
