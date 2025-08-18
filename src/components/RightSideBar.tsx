1fbb8941c986576e76ebe04cd7921f9943e80da5
Vibudh08 <vibudhrathore08@gmail.com>
2025-08-18 14:49:45 +0530

    chatbot content update

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -5,13 +5,13 @@ import ScrollToTop from "./ScrollToTopComponent";
 const RightSideBar = () => {
   return (
     <div className="fixed bottom-4 max-sm:bottom-20 right-1 max-sm:right-4 gap-2.5 flex flex-col justify-center text-center items-center">
-      <a
+      {/* <a
         href="https://wa.me/919717072701"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src="./WhatsApp.webp" className="w-14 max-sm:w-12 hover:scale-105" alt="" />
-      </a>
+      </a> */}
       <ChatBot />
       <ScrollToTop />
     </div>
